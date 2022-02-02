82a94fb8bfedd2d0fb0b904e01a77f3fbd7d6a85
ArtemE <qgren@ukr.net>
2022-02-02 22:31:30 +0200

    Completed 4 task

---
+++ Eremenko_Artem_dz_4/utils.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def currency_rates(code: str) -> float:
+    """возвращает курс валюты code по отношению к рублю"""
+    result = requests.get("http://www.cbr.ru/scripts/XML_daily.asp")  # Создание Get запроса на сайт
+    data_received_list = result.text.split('Valute')  # Сохранение полученных данных в список с удобным разбиванием
+    for i in range(len(data_received_list)):  # Перебор элементров списка пока не найдёт нужный
+        if code in data_received_list[i]:  # Проверка наличия в элементе списка нужной строки
+            list_item = data_received_list[i]  # Фиксация нужного элемента списка
+            start = list_item.find('<Value>')  # Поиск начала курса валюты
+            stop = list_item.find('</Value>')  # Поиск конца курса валюты
+            result_value = list_item[start + 7:stop]  # Фиксация элемента путём среза
+            result_value = float(result_value.replace(',', '.'))  # Получение float элемента с заменой запятой на точку
+            break  # Прерывание цикла для прекращения перебора
+        else:  # В случае если элемент пока не найден или не найдётся вовсе он даст вывод согласно поставленной задаче
+            result_value = None
+    return result_value
